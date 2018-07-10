fa975fa060fd085c755c9ccd114e186050834971
Zen <zenisbestwolf@gmail.com>
2018-07-10 17:45:26 -0400

    Fix permissions to be accurate to new roles.

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ client.on('message', message => {
 	if (command === undefined) return;
 	var userPermLevel = 0
 	if (message.channel.type === "text") {
-		if (message.member.roles.exists("name", "| • Trial Moderator • |") || message.member.roles.exists("name", "| • Moderator • |") || message.member.roles.exists("name", "| • Administrator • |")) userPermLevel = 1
-		if (message.member.roles.exists("name", "| • Mayor • |") || message.member.roles.exists("name", "| • Co-Mayors • |")) userPermLevel = 2
+		if (message.member.roles.exists("name", "Staff")) userPermLevel = 1
+		if (message.member.roles.exists("name", "Mayor") || message.member.roles.exists("name", "Co-Mayor")) userPermLevel = 2
 	};
 	if (message.author.id === zen) userPermLevel = 3
 	if (message.channel.id === "424197586879250444") {
